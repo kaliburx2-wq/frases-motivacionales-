d68017debaa7271986698e6344d03710f184110e
kalibur <kaliburx2@gmail.com>
2025-12-11 16:27:29 -0300

    Agrego base para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+   base: "/frases-motivacionales-/", // 👈 nombre EXACTO del repo
   plugins: [react()],
 })
